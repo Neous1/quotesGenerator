58564e12322815ae308d67cb0cc116d1568d8254
Neous1 <Neous1@users.noreply.github.com>
2017-08-08 23:05:53 -0400

    add getQuote function

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
 var quotes = [
     "Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.",
-"The way is not in the sky. The way is in the heart.",
-"You will not be punished for your anger, you will be punished by your anger.",
+    "The way is not in the sky. The way is in the heart.",
+    "You will not be punished for your anger, you will be punished by your anger.",
 
-"All that we are is the result of what we have thought. The mind is everything. What we think, we become.",
+    "All that we are is the result of what we have thought. The mind is everything. What we think, we become.",
 
-"Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one getting burned.",
+    "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one getting burned.",
 
 
-"The rule of friendship means there should be mutual sympathy between them, each supplying what the other lacks and trying to benefit the other, always using friendly and sincere words.",
+    "The rule of friendship means there should be mutual sympathy between them, each supplying what the other lacks and trying to benefit the other, always using friendly and sincere words.",
 
-"A good friend who points out mistakes and imperfections and rebukes evil is to be respected as if he reveals a secret of hidden treasure.",
+    "A good friend who points out mistakes and imperfections and rebukes evil is to be respected as if he reveals a secret of hidden treasure.",
 
-"The secret of health for both mind and body is not to mourn for the past, not to worry about the future, or not to anticipate troubles, but to live in the present moment wisely and earnestly."]
+    "The secret of health for both mind and body is not to mourn for the past, not to worry about the future, or not to anticipate troubles, but to live in the present moment wisely and earnestly."
+]
+
+
+function getQuote() {
+    console.log(quotes.length)
+    var quoteIndex = Math.floor(Math.random() * (quotes.length));
+    document.getElementById("quoteDisplay").innerHTML = quotes[quoteIndex];
+    console.log(quotes)
+
+}
